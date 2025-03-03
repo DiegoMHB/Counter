2ccd1855a347bc8df327061c1037580e52aea0e1
DiegoMHB <dmhb17@gmail.com>
2025-03-03 17:54:42 +0100

    success toast working!

--- counter/src/CounterComponent.tsx
+++ counter/src/CounterComponent.tsx
@@ -1,18 +1,28 @@
-import { useContext } from "react"
-import { CounterContext } from "./CounterContext"
-
+import { useContext } from "react";
+import { CounterContext } from "./CounterContext";
+import { useToast } from "@chakra-ui/react";
 
 export default function CounterComponent() {
-    const {count, increase} = useContext(CounterContext)
-   
+  const { count, increase } = useContext(CounterContext);
+  const toast = useToast()
+
   return (
-    <div style={{display:"flex",
-                gap:"10px"
-    }}>
-        <p>Current Count {count}:</p>
-        <button
-        onClick={increase}
-        >+1</button>
+    <div style={{ display: "flex", gap: "10px" }}>
+      <p>Current Count {count}:</p>
+      <button
+        onClick={() => {
+          increase();
+          toast({
+            title: 'Account created.',
+            description: "We've created your account for you.",
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+          })
+        }}
+      >
+        +1
+      </button>
     </div>
-  )
+  );
 }
