524b67c2ef5f0bcea98a7d172560d5a4b66cbc6f
DiegoMHB <dmhb17@gmail.com>
2025-03-03 18:09:37 +0100

    Toaster complete without styles

--- counter/src/CounterComponent.tsx
+++ counter/src/CounterComponent.tsx
@@ -1,28 +1,60 @@
 import { useContext } from "react";
 import { CounterContext } from "./CounterContext";
-import { useToast } from "@chakra-ui/react";
+import { Box, Button, useToast } from "@chakra-ui/react";
 
 export default function CounterComponent() {
   const { count, increase } = useContext(CounterContext);
-  const toast = useToast()
+  const toast = useToast();
 
   return (
-    <div style={{ display: "flex", gap: "10px" }}>
+    <div
+      style={{
+        display: "flex",
+        gap: "10px",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <p>Current Count {count}:</p>
-      <button
+      <Button
         onClick={() => {
           increase();
           toast({
-            title: 'Account created.',
-            description: "We've created your account for you.",
-            status: 'success',
-            duration: 9000,
-            isClosable: true,
-          })
+            position: "bottom-right",
+            render: () => (
+              <Box
+                position="relative"
+                display="inline-block"
+                p="5px"
+                borderRadius="8px"
+                background="linear-gradient(0deg, #6F7076, #6F7076), 
+                            radial-gradient(53.57% 282.15% at 2.14% 50%, 
+                            rgba(116, 200, 152, 0.65) 0%, 
+                            rgba(116, 200, 152, 0.1) 100%)"
+              >
+                <Box
+                  borderRadius="6px"
+                  background="gray.900"
+                  p="16px"
+                  color="white"
+                >
+                  <div style={{ display: "flex", alignItems:"center" , gap:"20px"}}>
+                    <div>V</div>
+                    <div>
+                      <p style={{ fontSize: "16px", fontWeight: "bold" }}>
+                        Incremented
+                      </p>
+                      <p>Counter is now {count}</p>
+                    </div>
+                  </div>
+                </Box>
+              </Box>
+            ),
+          });
         }}
       >
         +1
-      </button>
+      </Button>
     </div>
   );
 }
